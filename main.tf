82cfeb912832929b28b9128891dcf7e15d504620
Alastair Waddell <awaddell@nextops.com>
2022-04-25 14:23:38 +1000

    feat: add unifi-controller app

--- main.tf
+++ main.tf
@@ -13,4 +13,24 @@ provider "digitalocean" {
   # Configuration options
   token = var.do_token
 }
+
+resource "digitalocean_app" "unifi-controller" {
+  
+  spec {
+    name   = "unifi-controller"
+    region = "sfo"
+
+    service {
+      name               = "linuxserver-unifi-controller"
+      instance_count     = 1
+      instance_size_slug = "basic-xxs"
+
+      image {
+          registry      = "linuxserver"
+          registry_type = "DOCKER_HUB"
+          repository    = "unifi-controller"
+          tag           = "7.0.25"
+      }
+    }
+  }
 }
