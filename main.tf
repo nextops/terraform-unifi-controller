4a069dfb171b25304d35e7ec0673508a651e624e
Alastair Waddell <awaddell@users.noreply.github.com>
2022-05-02 12:20:36 +1000

    chore: rollback to 7.0.23

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "digitalocean_app" "unifi-controller" {
         registry      = "linuxserver"
         registry_type = "DOCKER_HUB"
         repository    = "unifi-controller"
-        tag           = "7.0.25"
+        tag           = "7.0.23"
       }
     }
   }
